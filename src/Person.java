42ee77b07344c6a54aba6f7d93bf8b3f93626433
Christian Kirschberg <ckirschberg@gmail.com>
2015-08-26 21:50:41 +0200

    person constructor

--- src/Person.java
+++ src/Person.java
@@ -2,4 +2,9 @@
  * Created by mariannenielsen on 26/08/15.
  */
 public class Person {
+
+    public Person()
+    {
+        System.out.println("I wonder who I am? I have no name.. Someone, please add a name :-)");
+    }
 }

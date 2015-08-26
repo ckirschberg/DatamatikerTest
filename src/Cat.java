546f87622f63a771f0860ad94d4c06c5699db482
Christian Kirschberg <ckirschberg@gmail.com>
2015-08-26 15:35:49 +0200

    Added cat class

---
+++ src/Cat.java
@@ -0,0 +1,19 @@
+/**
+ * Created by christiankirschberg on 26/08/15.
+ */
+public class Cat {
+    private String name;
+
+    public Cat(String name) {
+        setName(name);
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+}

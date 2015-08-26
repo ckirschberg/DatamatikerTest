af3d04555348a7c42d2b57a683dad32f3f46061b
MarianneNielsen <mn@kea.dk>
2015-08-26 21:53:25 +0200

    Method added

--- src/Person.java
+++ src/Person.java
@@ -2,4 +2,13 @@
  * Created by mariannenielsen on 26/08/15.
  */
 public class Person {
+
+    private String name;
+    private String title;
+
+
+    public String getName() {
+        this.name=name;
+        return name;
+    }
 }
